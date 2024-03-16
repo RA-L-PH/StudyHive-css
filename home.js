4fbcb669b8f2fad6dd347d25836a8fba0b667e72
RA-L-PH <123739852+RA-L-PH@users.noreply.github.com>
2024-03-16 12:24:09 +0530

    js uploaded

---
+++ home.js
@@ -0,0 +1,7 @@
+document.querySelector('.popup-trigger').addEventListener('click', function() {
+document.querySelector('.popup-window').style.display = 'block';
+  });
+  
+document.querySelector('.close-button').addEventListener('click', function() {
+document.querySelector('.popup-window').style.display = 'none';
+  });
