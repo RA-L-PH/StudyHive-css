e7ec22a5211b4dd1128490029a7df15295576964
RA-L-PH <123739852+RA-L-PH@users.noreply.github.com>
2024-05-02 08:51:51 +0530

    Update chatroom.js

--- chatroom.js
+++ chatroom.js
@@ -80,3 +80,12 @@ updateIntervalsButton.addEventListener('click', () => {
 });
 
 updateTimerDisplay();
+
+
+document.querySelector('.popup-trigger').addEventListener('click', function() {
+document.querySelector('.popup-window').style.display = 'block';
+  });
+  
+document.querySelector('.close-button').addEventListener('click', function() {
+document.querySelector('.popup-window').style.display = 'none';
+  });
